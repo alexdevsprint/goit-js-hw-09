f76022c67f543f05c0d970daecab4753cd0a15ba
alexdevsprint <alexdevsprint@gmail.com>
2025-02-24 15:00:50 +0000

    Deploying to gh-pages from @ alexdevsprint/goit-js-hw-09@4a9027176cc6c548d778372ece7ae2c8d9dee687 🚀

---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */document.querySelector(".feedback-form");document.addEventListener("input",t);function t(e){console.log(e.target.value)}
+//# sourceMappingURL=2-form.js.map
